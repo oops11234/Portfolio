9b99548c8c97fe8b00970948f08b1e625e7054a0
rick.lai@ambernet.com.tw <oops11234@gmail.com>
2025-05-10 00:01:19 +0800

    add hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Navbar from "./components/NavBar.tsx";
 import BlackJack from "./pages/BlackJack.tsx";
 import BlackjackTable from "./pages/BlackjackTable.tsx";
@@ -8,14 +8,14 @@ function App() {
 
   return (
     <>
-    <Router>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<div>這是首頁1</div>} />
         <Route path="/contract" element={<BlackJack />} />
         <Route path="/blackjackTable" element={<BlackjackTable />} />
       </Routes>
-    </Router>
+    </HashRouter>
     </>
   )
 }

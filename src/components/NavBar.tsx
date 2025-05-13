87b74a9ff98bc9735e4c468eecb6eea2061c3526
rick.lai@ambernet.com.tw <oops11234@gmail.com>
2025-05-13 22:22:27 +0800

    add light bar conditional statement

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,12 @@ import { Link,useLocation  } from "react-router-dom";
 
 export default function Navbar() {
   const location = useLocation();
-
-  const isIndexPage = location.pathname === "/";
+  const lightBarOn = ["/", '/liveStream'];
+  const isLightBarOne = lightBarOn.includes(location.pathname);
   return (
     <nav
       className={`bg-black p-4 flex justify-between items-center fixed h-[90px] top-0 left-0 w-full text-white z-40 ${
-        isIndexPage ? "shadow-lg shadow-cyan-500/30" : ""
+        isLightBarOne ? "shadow-lg shadow-cyan-500/30" : ""
       }`}
     >
       <Link to="/" className="text-4xl font-bold text-neon
